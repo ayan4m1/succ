77c436d1f119d257be1668f79d1b103ff65d5a0b
ayan4m1 <andrew@bulletlogic.com>
2022-03-22 23:53:24 -0400

    finish rewriting display code

--- firmware/lib/Display/Display.cpp
+++ firmware/lib/Display/Display.cpp
@@ -7,107 +7,60 @@ bmp_type gauge(gauge_size, gauge_buf);
 
 void redraw_gauge(const float percent) {
   gauge.clear(gauge.bounds());
-  draw::filled_rounded_rectangle(
-      gauge,
-      srect16(0, gauge.bounds().height() * (1 - percent),
-              gauge.bounds().width(), gauge.bounds().height()),
-      0.2, bmpa_color::green);
+  if (percent > 0) {
+    draw::filled_rounded_rectangle(
+        gauge,
+        srect16(0, gauge.bounds().height() * (1 - percent),
+                gauge.bounds().width(), gauge.bounds().height()),
+        0.2, bmpa_color::green);
+  }
   draw::rounded_rectangle(gauge, (srect16)gauge.bounds(), 0.2,
                           bmpa_color::gray);
 }
 
 bool Display::init() {
   redraw_gauge(0);
-  return lcd.initialize();
+
+  if (!lcd.initialize()) {
+    return false;
+  }
+
+  lcd.clear(lcd.bounds());
+  return true;
 }
 
-void Display::draw(const SensorReadings readings, const uint16_t rpm,
-                   const float speed) {
+void Display::draw(const uint16_t rpm, const float speed) {
   char* rpms = new char[6];
   sprintf(rpms, "%d", rpm);
-  auto text_size =
-      Bm437_ACM_VGA_9x14_FON.measure_text(ssize16(LCD_WIDTH, LCD_HEIGHT), rpms);
-  auto text_pos = srect16((LCD_WIDTH - text_size.width) / 2.0, LCD_HEIGHT - 14,
-                          LCD_WIDTH, LCD_HEIGHT);
 
+  const float text_scale = VeraMono_ttf.scale(32);
+  auto text_size = VeraMono_ttf.measure_text(ssize16(LCD_WIDTH, LCD_HEIGHT),
+                                             spoint16(0, 0), rpms, text_scale);
+  auto text_pos = srect16(
+      (LCD_WIDTH - text_size.width) / 2.0, LCD_HEIGHT - text_size.height,
+      (LCD_WIDTH - text_size.width) / 2.0 + text_size.width, LCD_HEIGHT);
+
+  uint8_t text_buf[bmp_type::sizeof_buffer((size16)text_size)];
+  bmp_type text((size16)text_size, text_buf);
+  text.clear(text.bounds());
+  draw::text(text, text_size.bounds(), spoint16(0, 0), rpms, VeraMono_ttf,
+             text_scale, lcd_color::ivory);
+
+  // update the gauge bitmap
   redraw_gauge(speed);
 
   draw::suspend(lcd);
 
-  // clear the whole screen
-  lcd.clear(lcd.bounds());
-  // draw the gauge
+  // draw the new gauge over the previous one
   draw::bitmap(
       lcd,
       srect16(GAUGE_X_PADDING, GAUGE_Y_PADDING, GAUGE_X_PADDING + GAUGE_WIDTH,
               GAUGE_Y_PADDING + GAUGE_HEIGHT),
       gauge, gauge.bounds());
+  // clear the area where the text lives
+  lcd.clear(rect16(0, LCD_HEIGHT - text_size.height, LCD_WIDTH, LCD_HEIGHT));
   // draw the rpm text
-  draw::text(lcd, text_pos, rpms, Bm437_ACM_VGA_9x14_FON, lcd_color::ivory,
-             lcd_color::black, false);
+  draw::bitmap(lcd, text_pos, text, text.bounds());
 
   draw::resume(lcd);
 }
-
-/*Display::Display() { display = new TFT_eSPI(SCREEN_WIDTH, SCREEN_HEIGHT); }
-
-Display::~Display() { free(display); }
-
-void Display::init() {
-  display->init();
-  display->fillScreen(TFT_BLACK);
-  display->setTextFont(0);
-}
-
-// uint16_t getBgColor(float iaq) {
-//   for (auto iterator = colorBands.begin(); iterator != colorBands.end();
-//        iterator++) {
-//     if (iaq < iterator->first) {
-//       return iterator->second;
-//     }
-//   }
-
-//   return TFT_RED;
-// }
-
-uint16_t Display::getBgColor(const float iaq) {
-  auto iterator = colorBands.begin();
-
-  // handle case where IAQ is very low
-  if (iaq < iterator->first) {
-    return iterator->second;
-  }
-
-  // linear interpolation of the two colors we are sitting between
-  for (iterator = colorBands.begin(); iterator != colorBands.end();
-       iterator++) {
-    const uint16_t startThreshold = iterator->first;
-    const uint16_t startColor = iterator->second;
-    uint16_t endColor = TFT_BLACK;
-    uint16_t endThreshold = iterator->first;
-
-    if (iterator != colorBands.end()) {
-      iterator++;
-      endColor = iterator->second;
-      endThreshold = iterator->first;
-    }
-
-    if (iaq < endThreshold && iaq >= startThreshold) {
-      const float theta =
-          ((iaq - startThreshold) / (endThreshold - startThreshold)) * 255;
-      return lerp16by8(startColor, endColor, theta);
-    }
-  }
-
-  // handle cases where IAQ is > 500
-  return TFT_BLACK;
-}
-
-void Display::draw(const SensorReadings readings, const uint16_t rpm) {
-  // todo: choose a bg color based on voc value
-  display->fillScreen(getBgColor(readings.iaq));
-  display->setTextColor(TFT_WHITE);
-  display->setTextSize(2);
-  display->setCursor(0, 0);
-  display->printf("%d rpm", rpm);
-}*/
--- firmware/lib/Display/Display.h
+++ firmware/lib/Display/Display.h
@@ -4,16 +4,12 @@
 #pragma once
 
 #include <Arduino.h>
+#include <Sensor.h>
+#include <VeraMono.h>
 
 #include <gfx_cpp14.hpp>
 #include <st7789.hpp>
 #include <tft_io.hpp>
-// #include <FastLED.h>
-#include <Sensor.h>
-// #include <TFT_eSPI.h>
-#include <Bm437_ACM_VGA_9x14.h>
-
-// #include <map>
 
 #define LCD_ROTATION 0
 #define LCD_WIDTH 135
@@ -28,7 +24,7 @@
 #define PIN_NUM_BCKL 4
 
 #define GAUGE_X_PADDING 8
-#define GAUGE_Y_PADDING 16
+#define GAUGE_Y_PADDING 32
 #define GAUGE_WIDTH LCD_WIDTH - (GAUGE_X_PADDING * 2)
 #define GAUGE_HEIGHT LCD_HEIGHT - (GAUGE_Y_PADDING * 2)
 
@@ -38,7 +34,7 @@ using namespace gfx;
 using bus_type = tft_spi_ex<LCD_HOST, PIN_NUM_CS, PIN_NUM_MOSI, PIN_NUM_MISO,
                             PIN_NUM_CLK, SPI_MODE0>;
 using lcd_type = st7789<LCD_WIDTH, LCD_HEIGHT, PIN_NUM_DC, PIN_NUM_RST,
-                        PIN_NUM_BCKL, bus_type, LCD_ROTATION, 400, 200>;
+                        PIN_NUM_BCKL, bus_type, LCD_ROTATION, true, 400, 200>;
 
 using lcd_color = color<typename lcd_type::pixel_type>;
 using bmp_type = bitmap<typename lcd_type::pixel_type>;
@@ -46,24 +42,11 @@ using bmp_color = color<typename bmp_type::pixel_type>;
 using bmpa_pixel_type = rgba_pixel<HTCW_MAX_WORD>;
 using bmpa_color = color<bmpa_pixel_type>;
 
-// const std::map<uint16_t, uint16_t> colorBands = {{50, TFT_GREEN},
-//                                                  {100, TFT_YELLOW},
-//                                                  {200, TFT_ORANGE},
-//                                                  {300, TFT_PURPLE},
-//                                                  {500, TFT_MAROON}};
-
 struct Display {
-  //   Display();
-  //   ~Display();
-
   bool init();
-  void draw(const SensorReadings readings, const uint16_t rpm,
-            const float speed);
+  void draw(const uint16_t rpm, const float speed);
 
  private:
-  uint16_t getBgColor(const float iaq);
-
-  // TFT_eSPI* display;
   lcd_type lcd;
 };
 
